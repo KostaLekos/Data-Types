ffb7ee2052d5490e55dc2081fe88552facacde37
KostaLekos <lekoskosta@proton.me>
2026-03-10 23:38:18 -0700

    Add insert method to BinaryTree

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,4 +1,4 @@
-public class BinaryTree<T> {
+public class BinaryTree<T extends Comparable<T>> {
     private Node<T> head;
 
     public BinaryTree(T head) {
@@ -9,11 +9,11 @@ public class BinaryTree<T> {
         this.head = null;
     }
 
-    public Node<T> get(T data) {
+    public Node<T> get(T value) {
         Node<T> current = head;
 
         while (current != null) {
-            int cmp = ((Comparable<T>) data).compareTo(current.data);
+            int cmp = value.compareTo(current.data);
             if (cmp == 0) return current;
             else if (cmp < 0) current = current.left;
             else current = current.right;
@@ -34,14 +34,40 @@ public class BinaryTree<T> {
                 currNode = currNode.right;
             } else {
                 throw new IllegalArgumentException(
-                    "Path must contain only a binary String. Invalid char: " + c
+                    "Path may only contain a binary String. Invalid char: " + c
                 );
             }
         }
         return currNode;
     }
 
-    public void insert() {
-        
+    public void insert(T value) {
+        if (head == null) {
+            head = new Node<>(value);
+            return;
+        }
+
+        Node<T> current = head;
+
+        while (true) {
+            int cmp = value.compareTo(current.data);
+            if (cmp == 0) {
+                return;
+            } else if (cmp < 0) {
+                if (current.left != null) {
+                    current = current.left;
+                } else {
+                    current.left = new Node<T>(value);
+                    return;
+                }
+            } else if (cmp > 0) {
+                if (current.right != null) {
+                    current = current.right;
+                } else {
+                    current.right = new Node<T>(value);
+                    return;
+                }
+            }
+        }
     }
 }
